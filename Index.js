11bed86c538325978a16af9d1ba7b6060ef00622
louiseisert <43786945+louiseisert@users.noreply.github.com>
2018-10-29 10:52:57 +0100

    sxs
    sxyyyxyx

---
+++ Index.js
@@ -0,0 +1,50 @@
+function calc(){
+  var n1 = parseInt(document.getElementById('n1').value);
+  var n11 = parseInt(document.getElementById('n11').value);
+  var n12 = parseInt(document.getElementById('n12').value);
+  var n13 = parseInt(document.getElementById('n13').value);
+  var n14 = parseInt(document.getElementById('n14').value);
+  var n15 = parseInt(document.getElementById('n15').value);
+  var n2 = parseInt(document.getElementById('n2').value);
+  var n21 = parseInt(document.getElementById('n21').value);
+  var n22 = parseInt(document.getElementById('n22').value);
+  var n23 = parseInt(document.getElementById('n23').value);
+  var n24 = parseInt(document.getElementById('n24').value);
+  var n25 = parseInt(document.getElementById('n25').value);
+
+  var n16 = document.getElementById('n16').value = n1 + n11 + n12 + n13 + n14 + n15;
+  var n26 = document.getElementById('n26').value = n2 + n21 + n22 + n23 + n24 + n25;
+
+  var n17 = parseInt(document.getElementById('n17').value);
+  var n27 = parseInt(document.getElementById('n27').value);
+
+  var n18 = document.getElementById('n18').value = n16+ n17;
+  var n28 = document.getElementById('n28').value = n26+ n27;
+
+  var n19 = parseInt(document.getElementById('n19').value);
+  var n191 = parseInt(document.getElementById('n191').value);
+  var n192 = parseInt(document.getElementById('n192').value);
+  var n193 = parseInt(document.getElementById('n193').value);
+  var n194 = parseInt(document.getElementById('n194').value);
+  var n195 = parseInt(document.getElementById('n195').value);
+  var n196 = parseInt(document.getElementById('n196').value);
+  var n198 = parseInt(document.getElementById('n198').value);
+
+  var n29 = parseInt(document.getElementById('n29').value);
+  var n291 = parseInt(document.getElementById('n291').value);
+  var n292 = parseInt(document.getElementById('n292').value);
+  var n293 = parseInt(document.getElementById('n293').value);
+  var n294 = parseInt(document.getElementById('n294').value);
+  var n295 = parseInt(document.getElementById('n295').value);
+  var n296 = parseInt(document.getElementById('n296').value);
+  var n298 = parseInt(document.getElementById('n298').value);
+
+
+  var n197 = document.getElementById('n197').value = n19 + n191 + n192 + n193 + n194 + n195 + n196;
+  var n198 = document.getElementById('n198').value = n16 + n17;
+  var n199 = document.getElementById('n199').value = n197 + n198;
+
+  var n297 = document.getElementById('n297').value = n29 + n291 + n292 + n293 + n294 + n295 + n296;
+  var n298 = document.getElementById('n298').value = n26 + n27;
+  var n299 = document.getElementById('n299').value = n297 + n298;
+}
